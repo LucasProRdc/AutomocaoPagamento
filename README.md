c3bdcaa3345421e71170e086c3e3ff286d3af713
Lucas Renan <lucascastelamareei@gmail.com>
2023-06-22 23:49:33 -0300

    login

---
+++ README.md
@@ -0,0 +1,1 @@
+Prohjeto de Automocao de pagamentos em restaurantes e padarias
---
+++ barbershop/customers/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Customers
+from .forms import CustomerForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def customer_list(request):
+
+    customers = Customers.objects.all()
+    print(customers)
+    return render(request, 'Customer.html', {'customers':customers})
+
+@login_required
+def customer_new(request):
+    
+    form = CustomerForm(request.POST, None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('customer_list')
+
+    return render(request, 'Customer_form.html', {'form':form})
+
+@login_required
+def customer_update(request, id):
+
+    customer = get_object_or_404(Customers, pk=id)
+    form = CustomerForm(request.POST or None, instance=customer)
+
+    if form.is_valid():
+        form.save()
+        return redirect('customer_list')
+
+    return render(request, 'Customer_form.html', {'form':form})
+
+@login_required
+def customer_delete(request, id):
+    customer = get_object_or_404(Customers, pk=id)
+    form = form = CustomerForm(request.POST or None, instance=customer)
+
+    if request.method == "POST":
+        customer.delete()
+        return redirect('customer_list')
+    
+    return render(request, 'Customer_form_delete.html', {'form':form})
