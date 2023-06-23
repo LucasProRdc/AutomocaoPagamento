9c0a939478315f8b3fface8829925792d9622be8
Lucas Renan <lucascastelamareei@gmail.com>
2023-06-22 23:53:58 -0300

    configuração

--- barbershop/products/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Products(models.Model):
-    description = models.CharField(max_length=30)
-    #picture
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    category = models.CharField(max_length=30)
-    
